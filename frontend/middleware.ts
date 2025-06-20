079827546b204e6f9f0531469b96ab4c913e43c4
david_bai <david_bai@example.com>
2025-06-20 23:06:59 +0800

    修复图片访问失败的问题

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,8 +1,8 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-import { i18n } from '@/constants/i18n-config'
-import { match as matchLocale } from '@formatjs/intl-localematcher'
-import Negotiator from 'negotiator'
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { i18n } from "@/constants/i18n-config";
+import { match as matchLocale } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
 
 function getLocale(request: NextRequest): string {
   // 1. 获取请求中的 Accept-Language
@@ -15,16 +15,16 @@ function getLocale(request: NextRequest): string {
 
   try {
     // 3. 匹配最佳语言
-    const locale = matchLocale(languages, locales, i18n.defaultLocale)
-    return locale
+    const locale = matchLocale(languages, locales, i18n.defaultLocale);
+    return locale;
   } catch (error) {
-    return i18n.defaultLocale
+    return i18n.defaultLocale;
   }
 }
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
-  
+
   // 获取所有的查询参数
   const searchParams = request.nextUrl.searchParams;
 
@@ -35,22 +35,26 @@ export function middleware(request: NextRequest) {
   // 如果路径中没有语言前缀,则重定向到带有语言前缀的路径
   if (pathnameIsMissingLocale) {
     const locale = getLocale(request);
-     // 创建新的 URL，保留原有的查询参数
-     const newUrl = new URL(`/${locale}${pathname}`, request.url);
-    
-     // 将原有的查询参数复制到新 URL
-     searchParams.forEach((value, key) => {
-       newUrl.searchParams.set(key, value);
-     });
- 
-     return NextResponse.redirect(newUrl);
+    // 创建新的 URL，保留原有的查询参数
+    const newUrl = new URL(`/${locale}${pathname}`, request.url);
+
+    // 将原有的查询参数复制到新 URL
+    searchParams.forEach((value, key) => {
+      newUrl.searchParams.set(key, value);
+    });
+
+    return NextResponse.redirect(newUrl);
   }
 }
 
 export const config = {
-  // 排除 public 文件、api 路由和 sitemap 相关路由
-  //排除了常见的静态资源文件扩展名（如 .png, .jpg, .gif 等），确保这些路径不会被中间件捕获
-  matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|\\.png$|\\.jpg$|\\.jpeg$|\\.gif$|\\.svg$).*)',
-  ],
+  /*
+   * Match all request paths except for the ones starting with:
+   * - api (API routes)
+   * - _next/static (static files)
+   * - _next/image (image optimization files)
+   * - favicon.ico (favicon file)
+   * - Or any path that contains a dot (e.g., .png, .jpg, .svg)
+   */
+  matcher: "/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)",
 };
